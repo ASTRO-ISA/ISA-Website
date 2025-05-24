b446fba467887dff4d941dcd28c483bf6d685242
Suyash Nagar <suyashnagar81@gmail.com>
2025-05-24 19:52:25 +0530

    backend login signup

--- server/app.js
+++ server/app.js
@@ -1,28 +1,23 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const cors = require('cors');
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const mongo_uri = process.env.MONGO_URL;
-
-// DB Connection
-mongoose.connect(mongo_uri)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+const express = require('express')
+const path = require('path')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const bodyParser = require('body-parser')
 
 // routes
-const createBlogRoute = require('./routes/createBlog.js');
+const createBlogRouter = require('./routes/createBlog.js')
+const userRouter = require('./routes/userRoutes.js')
 
 // middlewares
-app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uploads', express.static('uploads'));
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/uploads', express.static('uploads'))
 
-app.use('/create/blog', createBlogRoute);
+app.use('api/v1/create/blog', createBlogRouter)
+app.use('/api/v1/users', userRouter)
 
 // create blog
 // app.post('/create/blog', upload.single('thumbnail'), async (req, res) => {
