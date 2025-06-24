f6b8df2ff83885470dab62b610bd73f20e6c7688
Suyash Nagar <suyashnagar81@gmail.com>
2025-06-24 19:36:05 +0530

    -created bolgSuggestion Schema -created routes to do CURD operation over blogSuggestion -added restrictTo middleware to only allow admin to do changes -changed authenticateToken middleware so now req.user has the entire user object not only ignored -blogSuggestion functionality completed at backend

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -1,60 +1,64 @@
 const Blog = require('../models/blogModel')
 
 exports.allBlogs = async (req, res) => {
-    try {
-        const allBlogs = await Blog.find({}).populate('author', 'name country');
+  try {
+    const allBlogs = await Blog.find({}).populate('author', 'name country')
 
-        if(!allBlogs) {
-            res.status(404).json({message: 'Nothing to see here right now!'});
-        }
+    if (!allBlogs) {
+      res.status(404).json({ message: 'Nothing to see here right now!' })
+    }
 
-        res.status(200).json(allBlogs);
-    }
-    catch (err) {
-        res.status(500).json({message: 'Server Error'});
-    }
+    res.status(200).json(allBlogs)
+  } catch (err) {
+    res.status(500).json({ message: 'Server Error' })
+  }
 }
 
 exports.createBlog = async (req, res) => {
-    try {
-        const newBlog = new Blog({
-          thumbnail: req.file.filename,
-          title: req.body.title,
-          description: req.body.description,
-          content: req.body.content,
-          author: req.user.id
-        });
-        await newBlog.save();
-    
-        res.status(201).json({ message: "Blog saved successfully.", blog: newBlog })
-        console.log(newBlog)
-    } catch (err) {
-        res.status(500).json({ error: "Failed to save blog.", details: err.message });
-    }
+  try {
+    const newBlog = new Blog({
+      thumbnail: req.file.filename,
+      title: req.body.title,
+      description: req.body.description,
+      content: req.body.content,
+      author: req.user.id
+    })
+    await newBlog.save()
+
+    res.status(201).json({ message: 'Blog saved successfully.', blog: newBlog })
+    console.log(newBlog)
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: 'Failed to save blog.', details: err.message })
+  }
 }
 
 exports.featuredBlog = async (req, res) => {
-    try{
-        const featuredBlog = await Blog.findOne({featured:true}).populate('author', 'name country')
-        if(!featuredBlog){
-            return res.status(404).json({message: 'Blog not found'})
-        }
-        res.status(200).json(featuredBlog)
-    } catch (err){
-        res.status(500).json({message: 'server error in featured'})
+  try {
+    const featuredBlog = await Blog.findOne({ featured: true }).populate(
+      'author',
+      'name country'
+    )
+    if (!featuredBlog) {
+      return res.status(404).json({ message: 'Blog not found' })
     }
+    res.status(200).json(featuredBlog)
+  } catch (err) {
+    res.status(500).json({ message: 'server error in featured' })
+  }
 }
 
 exports.readBlog = async (req, res) => {
-    try {
-        const {id} = req.params;
-        const blog = await Blog.findById(id).populate('author', 'name country')
+  try {
+    const { id } = req.params
+    const blog = await Blog.findById(id).populate('author', 'name country')
 
-        if(!blog){
-            return res.status(404).json({message: 'Blog not found'})
-        }
-        res.status(200).json(blog)
-    } catch (err) {
-        res.status(500).json({ message: 'server error in readblog' });
+    if (!blog) {
+      return res.status(404).json({ message: 'Blog not found' })
     }
+    res.status(200).json(blog)
+  } catch (err) {
+    res.status(500).json({ message: 'server error in readblog' })
+  }
 }
--- server/middlewares/authenticateToken.js
+++ server/middlewares/authenticateToken.js
@@ -1,15 +1,31 @@
 const jwt = require('jsonwebtoken')
+const User = require('../models/userModel')
 require('dotenv').config()
-// const cookieParser = require('cookie-parser')
+
+const setUser = async (req, user) => {
+  try {
+    const userDoc = await User.findById(user.id)
+    if (!userDoc) return null
+    req.user = userDoc
+    // console.log('authenticate', req.user)
+    return true
+  } catch (error) {
+    console.error('Error setting user:', error)
+    return false
+  }
+}
 
 function authenticateToken(req, res, next) {
-  const token = req.cookies.jwt // extract token from cookie
+  const token = req.cookies.jwt
 
-  if (!token) return res.sendStatus(401)
+  if (!token) return res.status(401).json({ message: 'No token provided' })
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(401)
-    req.user = user
+  jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+    if (err) return res.status(401).json({ message: 'Invalid token' })
+
+    const success = await setUser(req, decoded)
+    if (!success) return res.status(401).json({ message: 'User not found' })
+
     next()
   })
 }
