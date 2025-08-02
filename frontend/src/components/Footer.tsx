818e1ba115a69df02ad355088b583572b6c19f11
Vinod Mali <snvinod09@gmail.com>
2025-08-02 23:25:51 +0530

    to pull main

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -37,8 +37,8 @@ const Footer = () => {
     }
     else{
       toast({
-        title: "Hold on!",
-        description: "You need to login to save blogs.",
+        title: "Login required!",
+        description: "Please login first to subscribe to our newsletter.",
         variant: "destructive",
       });
     }
